cbf42273dab4200380c4f40d14f165f9003b6428
Dimas <dimas.maulanads@gmail.com>
2019-12-06 11:31:08 +0700

    initial commit

---
+++ js/api.js
@@ -0,0 +1,93 @@
+const API_KEY = "16d85bf702974259b17e4dff4faeade4";
+const BASE_URL = "https://api.football-data.org/v2/";
+
+const LEAGUE_ID = 2021;
+
+const ENDPOINT_COMPETITION = `${BASE_URL}competitions/${LEAGUE_ID}/standings`;
+
+const fetchAPI = url => {
+    return fetch(url, {
+        headers: {
+            'X-Auth-Token': API_KEY
+        }
+    })
+        .then(res => {
+            if (res.status !== 200) {
+                console.log("Error: " + res.status);
+                return Promise.reject(new Error(res.statusText))
+            } else {
+                return Promise.resolve(res)
+            }
+        })
+        .then(res => res.json())
+        .catch(err => {
+            console.log(err)
+        })
+};
+
+function getAllStandings() {
+    if ("caches" in window) {
+        caches.match(ENDPOINT_COMPETITION).then(function (response) {
+            if (response) {
+                response.json().then(function (data) {
+                    console.log("Competition Data: " + data);
+                    showStanding(data);
+                })
+            }
+        })
+    }
+
+    fetchAPI(ENDPOINT_COMPETITION)
+        .then(data => {
+            showStanding(data);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+function showStanding(data) {
+    let standings = "";
+    let standingElement =  document.getElementById("homeStandings");
+
+    data.standings[0].table.forEach(function (standing) {
+        standings += `
+                <tr>
+                    <td><img src="${standing.team.crestUrl.replace(/^http:\/\//i, 'https://')}" width="30px" alt="badge"/></td>
+                    <td>${standing.team.name}</td>
+                    <td>${standing.won}</td>
+                    <td>${standing.draw}</td>
+                    <td>${standing.lost}</td>
+                    <td>${standing.points}</td>
+                    <td>${standing.goalsFor}</td>
+                    <td>${standing.goalsAgainst}</td>
+                    <td>${standing.goalDifference}</td>
+                </tr>
+        `;
+    });
+
+     standingElement.innerHTML = `
+                <div class="card" style="padding-left: 24px; padding-right: 24px; margin-top: 30px;">
+
+                <table class="striped responsive-table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Team Name</th>
+                            <th>W</th>
+                            <th>D</th>
+                            <th>L</th>
+                            <th>P</th>
+                            <th>GF</th>
+                            <th>GA</th>
+                            <th>GD</th>
+                        </tr>
+                     </thead>
+                    <tbody id="standings">
+                        ${standings}
+                    </tbody>
+                </table>
+                
+                </div>
+    `;
+}
---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "football-apps-v1";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/nav.html",
+    "/css/materialize.min.css",
+    "/js/api.js",
+    "/js/materialize.min.js",
+    "/js/nav.js",
+    "/pages/standing.html",
+];
+
+self.addEventListener("install", function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    const base_url = "https://api.football-data.org/v2/";
+    if (event.request.url.indexOf(base_url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function(cache) {
+                return fetch(event.request).then(function(response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
+                return response || fetch (event.request);
+            })
+        )
+    }
+});
